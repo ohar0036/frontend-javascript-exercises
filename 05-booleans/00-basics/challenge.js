d9639de843fb6fad337f923a56e7af10033ad26d
Marina <ohar0036@gmail.com>
2015-01-19 19:52:09 +0000

    Mocha Test Exercies

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,13 +1,13 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return (stringOne) == (stringTwo);
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return (one) !== (two);
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return (lower <= middle <= upper);
 };
 
 module.exports.outsideRanges = function(number) {
